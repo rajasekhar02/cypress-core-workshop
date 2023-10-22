93ace8ec8ac3a9282ed1e855d3ea25f5e6811649
rp6kp <rp6kp@umsystem.edu>
2023-10-22 17:28:07 -0500

    feat: add intercepts

--- cypress/e2e/09_intercepting_network_requests/demo_start.cy.js
+++ cypress/e2e/09_intercepting_network_requests/demo_start.cy.js
@@ -1,15 +1,24 @@
 /// <reference types="cypress" />
 
-it.only('board has no lists', () => {
+it('board has no lists', () => {
 
+  cy.intercept( 'GET', '/api/lists?boardId=1').as('listsAPI')
+  
   cy.visit('/board/1')
-
-  cy.get('[data-cy=list]')
+  
+  cy.wait('@listsAPI')
+  // cy.contains("Loading data...").should('not.exist') not working
+  
+  cy.get('[data-cy="list"]')
     .should('not.exist')
   
 });
 
-it('deleting a list', () => {
+it.only('deleting a list', () => {
+  cy.intercept({
+    method: 'DELETE',
+    url:"/api/lists/*"
+  }).as('deleteList')
 
   cy.visit('/board/1')
 
@@ -18,5 +27,6 @@ it('deleting a list', () => {
 
   cy.get('[data-cy="delete-list"]')
     .click()
-
+  cy.wait('@deleteList')
+  cy.get(`[data-cy="list"]`).should("not.exist")
 });
